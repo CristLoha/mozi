6ef70f3f3c039a909234b0f7cf0f67d6418dcb0d
Christ Henry Loha <cristloha2205@gmail.com>
2022-03-06 16:27:50 +0800

    add theme

---
+++ lib/shared/theme.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color kPrimaryColor = const Color(0xff546EE5);
+Color kBlackColor = const Color(0xff1D2027);
+Color kWhiteColor = const Color(0xffFFFFFF);
+Color kGreyColor = const Color(0xff696D74);
+
+TextStyle whiteTextStyle = GoogleFonts.poppins(
+  color: kWhiteColor,
+);
+TextStyle greyTextStyle = GoogleFonts.poppins(
+  color: kGreyColor,
+);
+
+TextStyle blueTextStyle = GoogleFonts.poppins(
+  color: kPrimaryColor,
+);
+
+FontWeight light = FontWeight.w300;
+FontWeight regular = FontWeight.w400;
+FontWeight medium = FontWeight.w500;
+FontWeight semiBold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
+FontWeight extraBold = FontWeight.w800;
+FontWeight black = FontWeight.w900;
